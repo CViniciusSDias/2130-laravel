ff7f99fd72ebbacfd5bcd36aef44d7e44f896d9f
Vinicius Dias <carlosv775@gmail.com>
2022-03-12 13:36:31 -0300

    Video 2.2

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -8,11 +8,13 @@ use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $series = Serie::query()->orderBy('nome')->get();
+        $mensagemSucesso = session('mensagem.sucesso');
 
-        return view('series.index')->with('series', $series);
+        return view('series.index')->with('series', $series)
+            ->with('mensagemSucesso', $mensagemSucesso);
     }
 
     public function create()
@@ -23,6 +25,7 @@ class SeriesController extends Controller
     public function store(Request $request)
     {
         Serie::create($request->all());
+        $request->session()->flash('mensagem.sucesso', 'Série adicionada com sucesso');
 
         return to_route('series.index');
     }
@@ -30,6 +33,7 @@ class SeriesController extends Controller
     public function destroy(Request $request)
     {
         Serie::destroy($request->series);
+        $request->session()->flash('mensagem.sucesso', 'Série removida com sucesso');
 
         return to_route('series.index');
     }
