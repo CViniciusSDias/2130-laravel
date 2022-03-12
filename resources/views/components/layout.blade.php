213a982a7b6f8b63ead1b9f64aa8547e39c65180
Vinicius Dias <carlosv775@gmail.com>
2022-03-12 14:40:34 -0300

    Video 3.2

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,6 +12,16 @@
 <div class="container">
     <h1>{{ $title }}</h1>
 
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     {{ $slot }}
 </div>
 </body>
--- resources/views/components/series/form.blade.php
+++ resources/views/components/series/form.blade.php
@@ -1,9 +1,9 @@
 <form action="{{ $action }}" method="post">
     @csrf
 
-    @isset($nome)
+    @if($update)
     @method('PUT')
-    @endisset
+    @endif
     <div class="mb-3">
         <label for="nome" class="form-label">Nome:</label>
         <input type="text"
