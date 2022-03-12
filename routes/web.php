d74824d65449f39f82a0046fb1455f88f1bf5ab3
Vinicius Dias <carlosv775@gmail.com>
2022-03-12 12:50:43 -0300

    Video 1.3

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/series');
 });
 
-Route::get('/series', [SeriesController::class, 'index']);
-Route::get('/series/criar', [SeriesController::class, 'create']);
-Route::post('/series/salvar', [SeriesController::class, 'store']);
+Route::controller(SeriesController::class)->group(function () {
+    Route::get('/series', 'index');
+    Route::get('/series/criar', 'create');
+    Route::post('/series/salvar', 'store');
+});
